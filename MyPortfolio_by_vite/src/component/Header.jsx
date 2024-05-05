ffd8d04fb8bf93a8ad440391df3afaa37f68d5fd
Dhairya Shah <dhairyaj2004@gmail.com>
2024-05-05 18:08:03 +0530

    new

--- MyPortfolio_by_vite/src/component/Header.jsx
+++ MyPortfolio_by_vite/src/component/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function Header() {
+ 
   return (
     <div className="bg-slate-400 flex justify-between px-4 py-3 lg:text-2xl md:text-xl">
         <div className="flex items-center"><img
@@ -11,18 +12,19 @@ export default function Header() {
           className="rounded-full  "
         />
         
-        <h3 className="font-playfair py-10 ml-4 lg:text-3xl md:text-2xl font-semibold underline underline-offset-8 text-slate-900">DHAIRYA SHAH</h3>
+        <h3 className="font-playfair py-10 ml-4 lg:text-3xl text-2xl font-semibold underline underline-offset-8 text-slate-900">DHAIRYA SHAH</h3>
         
-      </div>
-      <button data-collapse-toggle="navbar-default" type="button" className="mt-9 inline-flex items-center p-2 w-10 h-10 z-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+      </div >
+      {/* <div className="block lg:hidden md:hidden" id="toggle">
+      <button data-collapse-toggle="navbar-default" type="button" className="mt-9 flex items-center px-3 py-2  text-slate-900 border-slate-900">
         
         <span className="sr-only">Open main menu</span>
         <svg className="w-7 h-7" aria-hidden="true" xmlns="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png" fill="none" viewBox="0 0 17 14">
             <path stroke="Black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
         </svg>
-    </button>
+    </button></div> */}
       
-      <div className="py-10 hidden w-full md:block md:w-auto sm:w-auto" id="navbar-default">
+      <div className="py-10 hidden w-full md:block md:w-auto sm:w-auto" id="dropdown">
       <ul className="font-playfair flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-slate-400 md:dark:bg-slate-400 ">
         <li>
           <a href="#" className="block py-2 px-3 text-slate-900 rounded hover:bg-gray-100 hover:underline underline-offset-8  md:border-0  md:p-0  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">Home</a>
@@ -42,6 +44,8 @@ export default function Header() {
       </ul>
 
       </div>
+      
     </div>
+    
   );
 }
